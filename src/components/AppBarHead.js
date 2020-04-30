e7f98a3ded0c5bea64f0ed5ce3d0c6488e871142
VicCham98 <41102257+VicCham98@users.noreply.github.com>
2020-04-29 21:26:20 -0500

    searhBar append to appBar

--- src/components/AppBarHead.js
+++ src/components/AppBarHead.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Appbar } from 'react-native-paper';
+import { Searchbar } from 'react-native-paper';
 
 const theme = {
   colors: {
@@ -9,22 +10,41 @@ const theme = {
 }
 
 const AppBarHead = ({ navigation, title, icon }) => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const [openSearch, setOpenSearch] = useState(false)
+
   return (
     <Appbar.Header theme={theme} >
-        <Appbar.Action icon={icon} onPress={() => icon === 'menu' ? navigation.openDrawer() : navigation.goBack()} />
-        <Appbar.Content title={title} />
-        <Appbar.Action icon="magnify" onPress={() => navigation.openDrawer()} />
+      {
+        openSearch ?
+        <Fragment>
+          <Appbar.Action color='white' icon='arrow-left' onPress={() => setOpenSearch(!openSearch)} />
+          <View style={{ flex: 1 }}>
+            <Searchbar
+              placeholder="Search"
+              onChangeText={setSearchQuery}
+              value={searchQuery}
+            />
+          </View>
+        </Fragment>
+        :
+        <Fragment>
+          <Appbar.Action color='white' icon={icon} onPress={() => icon === 'menu' ? navigation.openDrawer() : navigation.goBack()} />
+          <Appbar.Content title={title} />
+          <Appbar.Action color='white' icon="magnify" onPress={() => setOpenSearch(!openSearch)} />
+        </Fragment>
+      }
       </Appbar.Header>
   );
 }
 
-// const styles = StyleSheet.create({
-//   bottom: {
-//     position: 'absolute',
-//     left: 0,
-//     right: 0,
-//     top: 0,
-//   },
-// });
+const styles = StyleSheet.create({
+  bottom: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+  },
+});
 
 export default AppBarHead;
